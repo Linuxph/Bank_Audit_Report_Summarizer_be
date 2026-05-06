fe1906a2226f6674be2ff30d1821e4b9286b2982
Linuxph <manassinghmakhija@gmail.com>
2026-05-07 00:50:54 +0530

    Fix properties for Render deployment

--- src/main/java/com/project/bars/BarsApplication.java
+++ src/main/java/com/project/bars/BarsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BarsApplication {
+public class    BarsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarsApplication.class, args);
